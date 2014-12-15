f7fd817229b0e4bbce423fda35c8cd6fc7c13f26
Kris Hall <tallgeese@localhost.localdomain>
2014-12-14 22:11:07 -0600

    added more to the generate_chain.vhd file

--- project/project_files/MP-1/generate_chain.vhd
+++ project/project_files/MP-1/generate_chain.vhd
@@ -21,15 +21,40 @@ architecture mixed of generate_chain is
 -------------------------------------------
 
 -------------------------------------------
--- 	signal declarations		 --
+--	type declarations		 --
 -------------------------------------------
-signal seed_in_reg	:	unsigned(255 downto 0);		--register for the seed value that is inputted
-signal candidate_reg	:	unsigned(255 downto 0);		--register to store the current candidate
+type generate_candidate_type is (IDLE, CAND_GEN, CHECK, PRINT);		--type for the different states for generating primes
+type prime_array is array (0 to 9) of unsigned(255 downto 0);		--array to store the confirmed prime numbers(maximum of ten)
+
+-------------------------------------------
+--   signal and register declarations	 --
+-------------------------------------------
+signal candidate_state		:	generate_candidate_type;	--current state of candidate generation
+signal candidate_state_next	:	generate_candidate_type;	--the next state in candidate generation
+
+--registers
+signal generation_complete	:	std_logic;			--register holding the value of whether or not generation of a candidate is complete
+signal is_prime			:	std_logic;			--register holding the value of whether or not a candidate is prime
+signal prime_counter		:	unsigned(3 downto 0);		--register that holds the number of 
+signal seed_in_reg		:	unsigned(255 downto 0);		--register for the seed value that is inputted
+signal candidate_reg		:	unsigned(255 downto 0);		--register to store the current candidate
+signal found_prime_array	:	prime_array;			--register to hold all the primes found with current seed
 
 begin
-	process(clk)
+	State_Update : process(clk)
 	begin
-	
-	end process;
+		if(rising_edge(clk)) then
+			if(reset = '1') then
+				candidate_state <= IDLE;
+			else
+				candidate_state <= candidate_state_next;
+			end if;
+		end if;
+	end process State_Update;
+
+	Next_State_Comp : process()
+	begin
+
+	end process Next_State_Comp;
 
 end mixed;
